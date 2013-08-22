52d7b4234e640e85b6ea2028d7794f733c6041b0
Bill Mers <bill@7x7labs.com>
2013-08-22 14:55:09 -0700

    [WIP] Updated chat view

--- whisper/ViewControllers/WHChatViewController.m
+++ whisper/ViewControllers/WHChatViewController.m
@@ -9,19 +9,21 @@
 #import "WHChatViewController.h"
 
 #import "Contact.h"
+#import "Message.h"
 #import "WHChatClient.h"
 
 #import <EXTScope.h>
 
 @interface WHChatViewController () <UITableViewDataSource, UITableViewDelegate, UITextFieldDelegate>
-@property (weak, nonatomic) IBOutlet UITextField *message;
+@property (weak, nonatomic) IBOutlet NSLayoutConstraint *keyboardHeight;
 @property (weak, nonatomic) IBOutlet UIButton *send;
 @property (weak, nonatomic) IBOutlet UITableView *chatLog;
-
+@property (weak, nonatomic) IBOutlet UITextField *message;
 @property (nonatomic, strong) NSArray *messages;
 @end
 
 @implementation WHChatViewController
+
 - (void)viewDidLoad {
     [super viewDidLoad];
     @weakify(self);
@@ -31,11 +33,12 @@
     RAC(self.messages) = [RACAbleWithStart(self.contact, messages)
                           map:^id(id value) {
                             return [value sortedArrayUsingDescriptors:@[[[NSSortDescriptor alloc]
-                                                                         initWithKey:@"sent" ascending:NO]]];
+                                                                         initWithKey:@"sent" ascending:YES]]];
                           }];
     [RACAble(self.messages) subscribeNext:^(id _) {
         @strongify(self)
         [self.chatLog reloadData];
+        [self scrollToEnd:self.chatLog];
     }];
 
     RAC(self.send, enabled) = [self.message.rac_textSignal
@@ -44,23 +47,66 @@
     self.chatLog.dataSource = self;
     self.chatLog.delegate = self;
     
-    [self.message becomeFirstResponder];
+    [[NSNotificationCenter defaultCenter] addObserver:self
+                                             selector:@selector(keyboardWillShow:)
+                                                 name:UIKeyboardWillShowNotification
+                                               object:nil];
+    
+    [[NSNotificationCenter defaultCenter] addObserver:self
+                                             selector:@selector(keyboardWillHide:)
+                                                 name:UIKeyboardWillHideNotification
+                                               object:nil];
+
+    [super viewDidLoad];
 }
 
 - (IBAction)sendMessage {
+    if ([self.message.text length] == 0)
+        return;
+    
     [self.client sendMessage:self.message.text to:self.contact];
     self.message.text = @"";
 }
 
 - (BOOL)textFieldShouldReturn:(UITextField *)textField {
-    [self.client sendMessage:self.message.text to:self.contact];
-    self.message.text = @"";
+    [self sendMessage];
     return NO;
+}
+
+- (void)keyboardWillShow:(NSNotification *)notification {
+    NSDictionary *info = [notification userInfo];
+    NSTimeInterval animationDuration = [info[UIKeyboardAnimationDurationUserInfoKey] doubleValue];
+
+    CGRect keyboardFrame = [info[UIKeyboardFrameEndUserInfoKey] CGRectValue];
+    CGFloat height = keyboardFrame.size.height;
+    
+    self.keyboardHeight.constant = height;
+    [self.view setNeedsUpdateConstraints];
+    
+    [UIView animateWithDuration:animationDuration animations:^{
+        [self.view layoutIfNeeded];
+    }];
+}
+
+- (void)keyboardWillHide:(NSNotification *)notification {
+    self.keyboardHeight.constant = 0;
+}
+
+- (void)scrollToEnd:(UIScrollView *)scrollView {
+    if (scrollView.contentSize.height > scrollView.frame.size.height)
+    {
+        CGPoint offset = CGPointMake(0, scrollView.contentSize.height - scrollView.frame.size.height);
+        [scrollView setContentOffset:offset animated:YES];
+    }
 }
 
 #pragma mark UITableViewDelegate
 - (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath {
     [self.messages[indexPath.row] delete];
+}
+
+-(void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
+    [self.message resignFirstResponder];
 }
 
 #pragma mark UITableViewDataSource
@@ -74,8 +120,27 @@
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
     UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"Cell" forIndexPath:indexPath];
-    cell.textLabel.text = [self.messages[indexPath.row] text];
+    Message *message = self.messages[indexPath.row];
+    
+    UILabel *messageLabel = (UILabel *)[cell viewWithTag:102];
+    messageLabel.text = message.text;
+    
+    UILabel *timestampLabel = (UILabel *)[cell viewWithTag:103];
+    timestampLabel.text = [self formatDate:message.sent];
+    
     cell.editing = YES;
     return cell;
 }
+
+- (NSString *)formatDate:(NSDate *)date {
+    NSDateFormatter *dateFormat = [[NSDateFormatter alloc] init];
+    [dateFormat setDateFormat:@"h:mma 'on' MMM d"];
+    
+    NSString *dateString;
+    dateString = [dateFormat stringFromDate:date];
+    dateString = [dateString lowercaseString];
+    
+    return dateString;
+}
+
 @end
