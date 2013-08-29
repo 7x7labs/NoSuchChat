58282ebead52b32ad9dbf268c778fcb46ebf2d1c
Bill Mers <bill@7x7labs.com>
2013-08-28 21:38:26 -0700

    [WIP] Differentiate incoming and outgoing messages in chat

--- whisper/ViewControllers/WHChatViewController.m
+++ whisper/ViewControllers/WHChatViewController.m
@@ -15,12 +15,13 @@
 
 #import <EXTScope.h>
 #import <SDWebImage/UIImageView+WebCache.h>
+#import "UIView+Position.h"
 
 @interface WHChatViewController () <UITableViewDataSource, UITableViewDelegate, UITextFieldDelegate>
-@property (weak, nonatomic) IBOutlet NSLayoutConstraint *keyboardHeight;
 @property (weak, nonatomic) IBOutlet UIButton *send;
 @property (weak, nonatomic) IBOutlet UITableView *chatLog;
 @property (weak, nonatomic) IBOutlet UITextField *message;
+@property (weak, nonatomic) IBOutlet UIView *inputView;
 @property (nonatomic, strong) NSArray *messages;
 @end
 
@@ -30,8 +31,8 @@
     [super viewDidLoad];
     @weakify(self);
 
-    self.title = self.contact.name;
-
+    RACBind(self.title) = RACBind(self.contact.name);
+    
     RAC(self.messages) = [RACAbleWithStart(self.contact, messages)
                           map:^id(id value) {
                             return [value sortedArrayUsingDescriptors:@[[[NSSortDescriptor alloc]
@@ -40,7 +41,7 @@
     [RACAble(self.messages) subscribeNext:^(id _) {
         @strongify(self)
         [self.chatLog reloadData];
-        [self scrollToEnd:self.chatLog];
+        [self scrollToBottom:self.chatLog animated:YES];
     }];
 
     RAC(self.send, enabled) = [RACSignal
@@ -53,13 +54,17 @@
     self.chatLog.dataSource = self;
     self.chatLog.delegate = self;
     
+    UITapGestureRecognizer *gestureRecognizer = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(hideKeyboard)];
+    gestureRecognizer.cancelsTouchesInView = NO;
+    [self.chatLog addGestureRecognizer:gestureRecognizer];
+    
     [[NSNotificationCenter defaultCenter] addObserver:self
-                                             selector:@selector(keyboardWillShow:)
+                                             selector:@selector(repositionForKeyboard:)
                                                  name:UIKeyboardWillShowNotification
                                                object:nil];
     
     [[NSNotificationCenter defaultCenter] addObserver:self
-                                             selector:@selector(keyboardWillHide:)
+                                             selector:@selector(repositionForKeyboard:)
                                                  name:UIKeyboardWillHideNotification
                                                object:nil];
 }
@@ -80,31 +85,37 @@
     return NO;
 }
 
-- (void)keyboardWillShow:(NSNotification *)notification {
+- (void)hideKeyboard {
+    [self.view endEditing:YES];
+}
+
+- (void)repositionForKeyboard:(NSNotification *)notification {
     NSDictionary *info = [notification userInfo];
     NSTimeInterval animationDuration = [info[UIKeyboardAnimationDurationUserInfoKey] doubleValue];
-
+    
     CGRect keyboardFrame = [info[UIKeyboardFrameEndUserInfoKey] CGRectValue];
     CGFloat height = keyboardFrame.size.height;
-    
-    self.keyboardHeight.constant = height;
-    [self.view setNeedsUpdateConstraints];
+
+    bool showing = [notification.name isEqualToString:@"UIKeyboardWillShowNotification"];
+    if (showing) height = -1 * height;
     
     [UIView animateWithDuration:animationDuration animations:^{
-        [self.view layoutIfNeeded];
+        self.inputView.frameY += height;
+        self.chatLog.frameHeight += height;
     }];
+    
+    [self scrollToBottom:self.chatLog animated:YES];
 }
 
-- (void)keyboardWillHide:(NSNotification *)notification {
-    self.keyboardHeight.constant = 0;
-}
-
-- (void)scrollToEnd:(UIScrollView *)scrollView {
-    if (scrollView.contentSize.height > scrollView.frame.size.height)
-    {
-        CGPoint offset = CGPointMake(0, scrollView.contentSize.height - scrollView.frame.size.height);
-        [scrollView setContentOffset:offset animated:YES];
-    }
+- (void)scrollToBottom:(UITableView *)table animated:(BOOL)animated {
+    int lastSection = [table numberOfSections] - 1;
+    int lastRow = [table numberOfRowsInSection:lastSection] - 1;
+    
+    if (lastSection == -1) return;
+    if (lastRow == -1) return;
+    
+    NSIndexPath *scrollIndexPath = [NSIndexPath indexPathForRow:lastRow inSection:lastSection];
+    [table scrollToRowAtIndexPath:scrollIndexPath atScrollPosition:UITableViewScrollPositionBottom animated:animated];
 }
 
 #pragma mark UITableViewDelegate
@@ -125,12 +136,31 @@
     return [self.messages count];
 }
 
-- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
-    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"Cell" forIndexPath:indexPath];
+// TODO: Refactor this .. it makes me cringe so much I don't even know where to begin.
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wdeprecated-declarations"
+- (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath
+{
     Message *message = self.messages[indexPath.row];
+    UIFont *font = [UIFont fontWithName:@"Helvetica Neue" size:15.0];
+
+    // iOS7 deprecated NSString sizeWithFont in favor of NSAttributedString boundingRectWithSize, however that method seems to
+    // ignore width contstraints.
+    CGSize size = [message.text sizeWithFont:font constrainedToSize:CGSizeMake(265, FLT_MAX) lineBreakMode:NSLineBreakByWordWrapping];
+    int height = size.height + 25;
+    if (height < 44) height = 44;
+
+    return height;
+}
+#pragma clang diagnostic pop
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    Message *message = self.messages[indexPath.row];
+    NSString *cellIdentifier = [message.incoming boolValue] ? @"IncomingChatCell" : @"OutgoingChatCell";
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:cellIdentifier forIndexPath:indexPath];
     
     // TODO: Better way to lookup the jid?
-    NSString *jid = ([message.incoming boolValue] ? message.contact.jid : self.client.jid);
+    NSString *jid = [message.incoming boolValue] ? message.contact.jid : self.client.jid;
     NSURL *avatarURL = [Contact avatarURLForEmail:jid];
 
     UIImageView *avatarImage = (UIImageView *)[cell viewWithTag:101];
@@ -138,20 +168,23 @@
     
     UILabel *messageLabel = (UILabel *)[cell viewWithTag:102];
     messageLabel.text = message.text;
+    messageLabel.frameWidth = 265;
     
     UILabel *timestampLabel = (UILabel *)[cell viewWithTag:103];
     timestampLabel.text = [self formatDate:message.sent];
+    timestampLabel.frameY = cell.frameHeight - 18;
 
 //    UIImageView *bubbleImage = (UIImageView *)[cell viewWithTag:104];
 //    bubbleImage.image = [[UIImage imageNamed:@"CKBubbleLeft"] stretchableImageWithLeftCapWidth:23 topCapHeight:15];
     
     cell.editing = YES;
+
     return cell;
 }
 
 - (NSString *)formatDate:(NSDate *)date {
     NSDateFormatter *dateFormat = [[NSDateFormatter alloc] init];
-    [dateFormat setDateFormat:@"h:mma ' on' MMM d"];
+    [dateFormat setDateFormat:@"h:mma MMM d"];
     
     NSString *dateString;
     dateString = [dateFormat stringFromDate:date];
