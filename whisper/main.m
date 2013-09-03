97b43f923485a2001fa4e0d0ff53ca81537891f7
Thomas Goyne <thomas@7x7labs.com>
2013-09-03 10:51:36 -0700

    Monkeypatch away the IOKit assertion spam

--- whisper/main.m
+++ whisper/main.m
@@ -10,8 +10,17 @@
 
 #import "WHAppDelegate.h"
 
-int main(int argc, char * argv[])
-{
+static int (*oldStderrWrite)();
+
+static int stderrWrite(void *inFD, const char *buffer, int size) {
+    if (strncmp(buffer, "AssertMacros:", 13) == 0)
+        return 0;
+    return oldStderrWrite(inFD, buffer, size);
+}
+
+int main(int argc, char *argv[]) {
+    oldStderrWrite = stderr->_write;
+    stderr->_write = stderrWrite;
     @autoreleasepool {
         return UIApplicationMain(argc, argv, nil, NSStringFromClass([WHAppDelegate class]));
     }
